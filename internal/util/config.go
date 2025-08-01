d347deadd5ed77d567cd18118dbc2866a89b81c2
Sobhan Aali <sobhanaali80@gmail.com>
2025-08-01 23:24:27 +0330

    database connection

--- internal/util/config.go
+++ internal/util/config.go
@@ -3,9 +3,16 @@ package util
 import "github.com/spf13/viper"
 
 type Config struct {
-	APPPROT string `mapstructure:"APP_PRORT"`
-	APPNAME string `mapstructure:"APP_NAME"`
-	APPDEBUG string `mapstructure:"APP_DEBUG"`
+	AppPort      string `mapstructure:"APP_PORT"`
+	AppName      string `mapstructure:"APP_NAME"`
+	AppDebug     string `mapstructure:"APP_DEBUG"`
+
+	DBConnection string `mapstructure:"DB_CONNECTION"`
+	DBHost       string `mapstructure:"DB_HOST"`
+	DBUsername   string `mapstructure:"DB_USERNAME"`
+	DBPassword   string `mapstructure:"DB_PASSWORD"`
+	DBDatabase   string `mapstructure:"DB_DATABASE"`
+	DBPort       string `mapstructure:"DB_PORT"`
 }
 
 func LoadConfig(path string) (config Config , err error) {
--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"os"
+
 	"github.com/Sobhanaali/Bus-Ticket/internal/util"
+	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 )
@@ -16,7 +20,32 @@ func main() {
 		log.Fatal().Err(err).Msg("Cannot load config !")
 	}
 
-	if(config.APPDEBUG == "true") {
+	if(config.AppDebug == "true") {
 		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 	}
+
+	dsn := fmt.Sprintf(
+		"%s://%s:%s@%s:%s/%s",
+		config.DBConnection,
+		config.DBUsername,
+		config.DBPassword,
+		config.DBHost,
+		config.DBPort,
+		config.DBDatabase,
+	)
+
+	dbpool, err := pgxpool.New(context.Background(), dsn)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to create connection pool: %v\n", err)
+		os.Exit(1)
+	}
+	
+	err = dbpool.Ping(context.Background())
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "❌ Cannot connect to database: %v\n", err)
+		os.Exit(1)
+	}
+
+	defer dbpool.Close()
 }
