1f8aa06b1b341d95e6eaf382f1fb299289f35810
Sobhan Aali <sobhanaali80@gmail.com>
2025-08-01 20:32:46 +0330

    Add zerolog

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
-import "github.com/Sobhanaali/Bus-Ticket/internal/util"
+import (
+	"os"
+	"github.com/Sobhanaali/Bus-Ticket/internal/util"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
 
 func main() {
 	// load config
 
 	config , err := util.LoadConfig(".")
 
-	
+	if(err != nil) {
+		log.Fatal().Err(err).Msg("Cannot load config !")
+	}
+
+	if(config.APPDEBUG == "true") {
+		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+	}
 }
